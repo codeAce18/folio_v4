3343d87a1f0e3db5a3d49e449a841ea9ca0f523b
TechMogul <joeltaiwo20018@gmail.com>
2025-02-01 19:42:41 +0100

    first commit

--- src/js/components/Time.js
+++ src/js/components/Time.js
@@ -8,6 +8,7 @@ export default class Time extends Component {
       elements: {
         hour: "[data-hour]",
         minute: "[data-minute]",
+        period: "[data-period]", // Add period element
       },
     });
 
@@ -15,16 +16,19 @@ export default class Time extends Component {
 
     this.oldHour = this.formattedTime.hourValue;
     this.oldMinute = this.formattedTime.minuteValue;
+    this.oldPeriod = this.formattedTime.period; // Track old period
 
-    setInterval(this.updateTime, 1000);
+    setInterval(this.updateTime.bind(this), 1000); // Bind `this` to the method
   }
 
   get currentTime() {
     const options = {
       hour: "numeric",
       minute: "numeric",
-      // second: "numeric",
-      timeZone: "Europe/London",
+      // Enable AM/PM
+      hour12: true, 
+      // Lagos timezone
+      timeZone: "Africa/Lagos",
     };
 
     const time = new Intl.DateTimeFormat([], options).format(new Date());
@@ -33,20 +37,20 @@ export default class Time extends Component {
   }
 
   get formattedTime() {
-    const time = this.currentTime;
-    const timeArray = time.split(":");
-    const hourValue = timeArray[0];
-    const minuteValue = timeArray[1];
+    const time = this.currentTime; // e.g., "2:30 PM"
+    const [timePart, period] = time.split(" "); // Split into "2:30" and "PM"
+    const [hourValue, minuteValue] = timePart.split(":"); // Split into "2" and "30"
 
     return {
       hourValue,
       minuteValue,
+      period, // Include period (AM/PM)
     };
   }
 
   updateTime() {
-    const { hour, minute } = this.elements;
-    const { hourValue, minuteValue } = this.formattedTime;
+    const { hour, minute, period } = this.elements; // Add period element
+    const { hourValue, minuteValue, period: newPeriod } = this.formattedTime; // Destructure period
 
     mapEach(hour, (element) => {
       if (this.oldHour !== hourValue) {
@@ -75,7 +79,23 @@ export default class Time extends Component {
       }
     });
 
+    // Update the period (AM/PM)
+    mapEach(period, (element) => {
+      if (this.oldPeriod !== newPeriod) {
+        element.classList.add("flash");
+
+        setTimeout(() => {
+          element.innerHTML = newPeriod;
+        }, 500);
+
+        setTimeout(() => {
+          element.classList.remove("flash");
+        }, 1000);
+      }
+    });
+
     this.oldHour = hourValue;
     this.oldMinute = minuteValue;
+    this.oldPeriod = newPeriod; // Track the old period for comparison
   }
 }
